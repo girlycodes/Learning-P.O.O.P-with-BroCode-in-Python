6d8bc745673ed1c3d925139e6fe056a51f8e9474
girlycodes <languageszara@gmail.com>
2025-05-20 19:39:36 -0300

    criando a primeira classe

---
+++ Conceitos/Introdução/classe e código/classe.py
@@ -0,0 +1,9 @@
+# Neste arquivo, escreveremos apenas a nossa classe!
+
+
+class Carro: # a classe a ser criada deve sempre começar com letras maiúsculas  
+    def __init__(self, cor, modelo, ano, esta_vendendo): # dentre parênteses está os atributos que nossos objetos terão
+        self.cor = cor # atribuindo nossos atributos
+        self.modelo = modelo
+        self.ano = ano
+        self.esta_vendendo = esta_vendendo
---
+++ Conceitos/Introdução/classe e código/código.py
@@ -0,0 +1,6 @@
+# Já neste, escreveremos nossos códigos para usarmos nossa classe
+
+from classe import Carro
+
+carro1 = Carro("Mustang", "Laranja",  2025, False)
+print(carro1.cor)
